de8ff58ec2aae519d9b07769f705a266bd5aa0d9
ahnge <ahnge226@gmail.com>
2022-11-29 12:45:50 +0630

    codevolution testing tut init

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,7 @@
-import { isConditionalExpression } from "typescript";
 import "./App.css";
-import Button from "./components/html/Button";
-// import Toast from "./components/templatelaterals/Toast";
-// import Random from "./components/random/Random";
-// import List from "./components/generic/List";
-// import Private from "./components/auth/Private";
-// import Profile from "./components/auth/Profile";
-// import User from "./components/context/User";
-// import { UserContextProvider } from "./components/context/UserContext";
-// import { ThemeContextProvider } from "./components/context/ThemeContext";
-// import Box from "./components/context/Box";
-// import Counter from "./components/state/Counter";
-// import Container from "./components/Container";
-// import User from "./components/state/User";
-// import Input from "./components/Input";
-// import Heading from "./components/Heading";
-// import Oscar from "./components/Oscar";
-// import Status from "./components/Status";
-// import Greet from "./components/Greet";
-// import Button from "./components/Button";
-// import Person from "./components/Person";
-// import PersonList from "./components/PersonList";
 
 function App() {
-  // const name = {
-  //   first: "Bruce",
-  //   last: "Wayne",
-  // };
-  // const names = [
-  //   {
-  //     first: "Bruce",
-  //     last: "Wayhne",
-  //   },
-  //   {
-  //     first: "Princess",
-  //     last: "Diana",
-  //   },
-  //   {
-  //     first: "Cleark",
-  //     last: "cankt",
-  //   },
-  // ];
-  return (
-    <div className="App">
-      <Button varient="primaty" onClick={() => console.log("hello")}>
-        Primary btn
-      </Button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/components/auth/Profile.tsx
+++
@@ -1,9 +0,0 @@
-export type ProfileType = {
-  name: string;
-};
-
-const Profile = ({ name }: ProfileType) => {
-  return <div>User profile. the name is {name}</div>;
-};
-
-export default Profile;
